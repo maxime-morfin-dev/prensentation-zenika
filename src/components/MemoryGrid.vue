d094e5751f7b7fea15cba7341be3a5d7b8f31adb
Maxime Morfin <maxime@MBP-de-Maxime.lan>
2023-04-22 21:42:05 +0200

    make loop for card

--- src/components/MemoryGrid.vue
+++ src/components/MemoryGrid.vue
@@ -1,17 +1,18 @@
-<script setup lang="ts">
-import MemoryCard from './MemoryCard.vue';
-</script>
 <template>
    <div class="grid grid-cols-2 gap-8">
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
-      <MemoryCard />
+      <MemoryCard  v-for="(card, index) in cards" key="card.index"/>
    </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref, type DefineComponent, type Ref } from 'vue';
+import MemoryCard from './MemoryCard.vue';
+
+const cards:Ref<Array<any>> = ref([])
+
+onMounted(() => {
+   for(let i=0; i<=16; i++){
+      cards.value.push(i)
+   }
+})
+</script>
