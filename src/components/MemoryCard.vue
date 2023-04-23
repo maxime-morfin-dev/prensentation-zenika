41a5bce9c1f8feb745ae4a15f8b075f1aa389d6f
Maxime Morfin <maxime@MBP-de-Maxime.lan>
2023-04-23 15:31:56 +0200

    implement check function logical

--- src/components/MemoryCard.vue
+++ src/components/MemoryCard.vue
@@ -1,10 +1,8 @@
 <template>
    <div @click="toggleCardShow(id)" class="cursor-pointer">
-      <div v-if="!cards[id!].show" class="bg-sky-300 bg-opacity-50 p-4 h-48 rounded-xl">
-         Hello memory card don't show
-      </div>
-      <div v-else class="bg-emerald-300 bg-opacity-50 p-4 h-48 rounded-xl">
-         Hello memory card show
+      <div v-if="!cards[id!].show" class="bg-sky-300 bg-opacity-50 p-4 h-48 rounded-xl" />
+      <div v-else class="bg-emerald-300 bg-opacity-50 p-4 h-48 rounded-xl flex items-center justify-center">
+         {{ cards[id!].value }}
       </div>
    </div>
 </template>
--- src/stores/memoryStore.ts
+++ src/stores/memoryStore.ts
@@ -1,8 +1,22 @@
 import { defineStore } from 'pinia'
-
+interface State {
+   cards: any,
+   pairChoice: PairChoiceInterface[] | any[],
+   winArray: WinArrayInterface[] | any[]
+}
+interface PairChoiceInterface {
+   id: number,
+   value: string,
+   show: boolean
+}
+interface WinArrayInterface {
+   id: number,
+   value: string,
+   show: boolean
+}
 export const useMemoryStore = defineStore('memory', {
   //state
-  state: () => ({
+  state: () : State => ({
     cards: [
       { id: 0, value: '🚀', show: false },
       { id: 1, value: '🚀', show: false },
@@ -10,17 +24,47 @@ export const useMemoryStore = defineStore('memory', {
       { id: 3, value: '🚤', show: false },
       { id: 4, value: '🚁', show: false },
       { id: 5, value: '🚁', show: false }
-    ]
+    ],
+    pairChoice: [],
+    winArray: [],
   }),
   //getters
   //actions
   actions : {
       toggleCardShow(id: number | undefined){
-         console.log("toggle");
-         
-         const card = this.cards.find((card) => card.id == id)
-         if(card) {
+         if(this.cards[id!].show){
+            return
+         }
+         const card = this.cards.find((card: { id: number | undefined; }) => card.id == id)
+         if(card &&  !this.winArray.includes(card)) {
             card.show = !card.show
+            this.checkFunction(card)
+         }
+      },
+      checkFunction(card:{id: number, value: string, show: boolean}){
+         if(this.pairChoice.length == 0){
+            //If array empty add first choice
+            this.pairChoice.push(card)
+         }else{
+            console.log('else');
+            
+            if(this.pairChoice.length == 2){
+               console.log('la');
+               
+               this.pairChoice.forEach(pair => {
+                  this.cards[pair.id].show = false
+               });
+               this.pairChoice = [card]
+            }else{
+               console.log('else else');
+               
+               if(this.pairChoice[0].value == card.value){
+                  this.winArray.push(...arguments, this.pairChoice[0], this.pairChoice[1])
+                  this.pairChoice= []
+               }else{
+                  this.pairChoice.push(card)
+               }
+            }
          }
       }
   }
