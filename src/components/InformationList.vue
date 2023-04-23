e745e17c29621a259d0ca737e2cb339530b5277d
Maxime Morfin <maxime@MBP-de-Maxime.lan>
2023-04-23 18:45:45 +0200

    show informations title only if winArray is not empty

--- src/components/InformationList.vue
+++ src/components/InformationList.vue
@@ -1,5 +1,5 @@
 <template>
-   <h2>List des information a mon sujet</h2>
+   <h2 v-show="winArray.length > 0">List des information a mon sujet</h2>
    <div v-for="(information, index) in informations" :key="index">
       <div v-show="information.show">
          {{ information.value }}
@@ -8,7 +8,10 @@
 </template>
 <script setup lang="ts">
    import { useInformationStore } from '@/stores/informationStore'
+   import { useMemoryStore } from '@/stores/memoryStore';
 
    const store = useInformationStore()
+   const memoryStore = useMemoryStore()
    const {informations} = store
+   const {winArray} = memoryStore
 </script>
