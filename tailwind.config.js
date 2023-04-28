d93c04cac052e103f9df6aeb8cb5b325ab2c8601
Maxime Morfin <maxime@MacBook-Pro-de-Maxime.local>
2023-04-28 20:54:27 +0200

    modify tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+/** @type {import/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
